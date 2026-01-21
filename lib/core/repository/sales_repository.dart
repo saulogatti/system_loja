70eb4d738fa16654a5a5f51359d8eaba05455551
Saulo Gatti <saulogatti@gmail.com>
2026-01-20 22:48:01 -0300

    feat: Adicionar documentação de arquitetura e migração do banco de dados, além de implementar melhorias no repositório de vendas

--- lib/core/repository/sales_repository.dart
+++ lib/core/repository/sales_repository.dart
@@ -1,6 +1,6 @@
 import 'package:system_loja/core/models/invoice.dart';
-import 'package:system_loja/data/database/app_database.dart';
 import 'package:system_loja/data/database/dao/invoice_dao.dart';
+import 'package:system_loja/screens/injection/app_injection.dart';
 
 /// Repositório para gerenciamento de vendas usando Drift
 ///
@@ -10,7 +10,7 @@ class SalesRepository {
   final InvoiceDao _invoiceDao;
 
   SalesRepository({InvoiceDao? invoiceDao})
-    : _invoiceDao = invoiceDao ?? AppDatabase().invoiceDao;
+    : _invoiceDao = invoiceDao ?? AppInjection.instance.appDatabase.invoiceDao;
 
   /// Deleta uma venda pelo ID
   ///
