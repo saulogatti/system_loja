ab71ea555c7bba84fa377aefdb7674fc3035fc70
Saulo Gatti <saulogatti@gmail.com>
2026-02-09 02:21:49 -0300

    feat: Enhance SystemConfiguration and SystemConfigWidget for improved configuration management
    - Added copyWith method to SystemConfiguration for easier object cloning with updated fields.
    - Updated SystemConfigWidget to manage payment method selection and save configuration data.
    - Refactored UI components to streamline the editing process for payment types and product categories.

--- lib/screens/configuracoes/widgets/system_config_widget.dart
+++ lib/screens/configuracoes/widgets/system_config_widget.dart
@@ -1,25 +1,22 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:system_loja/core/models/system_config/price_configuration.dart';
 import 'package:system_loja/screens/configuracoes/bloc/configuration_data_cubit.dart';
 import 'package:system_loja/screens/configuracoes/bloc/configuration_data_state.dart';
 
 /// Widget para configurar as dados padrão do sistema
 /// Cadastrar dados como tipos de pagamento, categorias de produtos, etc. para facilitar o uso do sistema.
+@RoutePage()
 class SystemConfigWidget extends StatefulWidget {
-  final DateTime? initialData;
-  final Function(DateTime) onDataChanged;
-
-  const SystemConfigWidget({
-    super.key,
-    this.initialData,
-    required this.onDataChanged,
-  });
+  const SystemConfigWidget({super.key});
 
   @override
   State<SystemConfigWidget> createState() => _SystemConfigWidgetState();
 }
 
 class _SystemConfigWidgetState extends State<SystemConfigWidget> {
+  List<PaymentMethodType> selectedPaymentMethods = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,25 +33,55 @@ class _SystemConfigWidgetState extends State<SystemConfigWidget> {
               trailing: IconButton(
                 icon: const Icon(Icons.edit),
                 onPressed: () {
-                  // Lógica para editar os tipos de pagamento
-                  
+                  // Lógica para editar os tipos de pagamento [PaymentMethodType]
+                  showDialog(
+                    context: context,
+                    builder: (context) => Dialog(
+                      child: Column(
+                        children: [
+                          Text('Selecione os tipos de pagamentos'),
+                          ListView.builder(
+                            itemCount: PaymentMethodType.values.length,
+                            itemBuilder: (context, index) => CheckboxListTile(
+                              title: Text(PaymentMethodType.values[index].name),
+                              value: data.priceConfiguration.types.contains(
+                                PaymentMethodType.values[index],
+                              ),
+                              onChanged: (value) {
+                                if (value == true) {
+                                  selectedPaymentMethods.add(
+                                    PaymentMethodType.values[index],
+                                  );
+                                } else {
+                                  selectedPaymentMethods.remove(
+                                    PaymentMethodType.values[index],
+                                  );
+                                }
+                              },
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                              context
+                                  .read<ConfigurationDataCubit>()
+                                  .saveConfigurationData(
+                                    data.copyWith(
+                                      priceConfiguration: PriceConfiguration(
+                                        types: selectedPaymentMethods,
+                                      ),
+                                    ),
+                                  );
+                            },
+                            child: const Text('Salvar'),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
                 },
               ),
             ),
-
-            // Categoria de produtos
-            ListTile(
-              title: const Text('Categorias de Produtos'),
-              subtitle: Text(data.productCategories.join(', ')),
-              trailing: IconButton(
-                icon: const Icon(Icons.edit),
-                onPressed: () {
-                  // Lógica para editar as categorias de produtos
-                },
-              ),
-            ),
-
-            // Adicione mais configurações de dados padrão aqui
           ],
         ),
       ),
