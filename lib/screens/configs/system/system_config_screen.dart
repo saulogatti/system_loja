94397f0312e68181ffd76d81fd132c7680505d80
Saulo Gatti <saulogatti@outlook.com>
2026-05-30 21:45:48 -0300

    🎨 Ajustar campo de entrada para aceitar apenas números no período padrão

--- lib/screens/configs/system/system_config_screen.dart
+++ lib/screens/configs/system/system_config_screen.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:system_loja/aplication/app_injection.dart';
 import 'package:system_loja/core/interface/i_system_repository.dart';
@@ -301,12 +302,13 @@ class _SystemConfigScreenState extends State<SystemConfigScreen> {
             TextField(
               controller: _defaultPeriodController,
               keyboardType: TextInputType.number,
+              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
               textInputAction: TextInputAction.done,
               decoration: const InputDecoration(
                 labelText: 'Período padrão (dias)',
                 border: OutlineInputBorder(),
               ),
-            ),
+            )
           ],
         ),
       ),
